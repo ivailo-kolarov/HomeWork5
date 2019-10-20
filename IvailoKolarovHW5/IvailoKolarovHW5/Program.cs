bf6be48250fa888e0a5c501b4f2821e58c105cb9
Ivailo Kolarov <kolarov.ivailo@gmail.com>
2019-10-20 16:54:59 +0300

    HomeWork

---
+++ IvailoKolarovHW5/IvailoKolarovHW5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IvailoKolarovHW5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentense = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
+            string[] words = sentense.Split(' ') ;
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+           
+        }
+    }
+}
